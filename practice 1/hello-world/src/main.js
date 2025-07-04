526322d4c11418e7deb0f841b8b75831e8cdf8b9
Akshit Sharma <akshitgamerz018@gmail.com>
2025-07-04 04:46:15 +0000

    added animation

--- practice 1/hello-world/src/main.js
+++ practice 1/hello-world/src/main.js
@@ -4,38 +4,61 @@ import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 const scene =new THREE.Scene()
 
 const cubeGeometry=new THREE.BoxGeometry(1,1,1)
-const cubeMaterial= new THREE.MeshBasicMaterial({color:"red"})
+const cubeMaterial= new THREE.MeshBasicMaterial({color:"red", wireframe:true})
 const cubeMesh =new THREE.Mesh(
   cubeGeometry,
   cubeMaterial
 )
-
+cubeMesh.position.x = 1
+cubeMesh.position.y = 1
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper)
 scene.add(cubeMesh)
 
 console.log(cubeMesh);
 console.log(scene);
 
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,30)
-
-camera.position.z=5
+console.log(camera)
+camera.position.z=5   
 scene.add(camera)
 
 
 const canvas = document.querySelector('canvas.threejs')
 console.log(canvas);
 const renderer =new THREE.WebGLRenderer({
-  canvas:canvas
+  canvas:canvas,
+  antialias:true
 })
 
 renderer.setSize(window.innerWidth,window.innerHeight)
-const controls = new OrbitControls(camera,canvas)
-controls.autoRotate=true
-controls.enableDamping=true
+renderer.setPixelRatio(window.devicePixelRatio)
 
+const controls = new OrbitControls(camera,canvas)
+controls.autoRotate=false
+controls.enableDamping=true
+camera.aspect=window.innerWidth/window.innerHeight;
+
+window.addEventListener('resize',()=>{
+  camera.aspect=window.innerWidth/window.innerHeight;
+  renderer.setSize(window.innerWidth,window.innerHeight)
+  camera.updateProjectionMatrix();
+  
+})
+const clock= new THREE.Clock()
+let previousTime=0;
+console.log(window.devicePixelRatio)
 const renderloop =()=>{
+  const CurrentTime = clock.getElapsedTime()
+  const delta = CurrentTime - previousTime
+  previousTime = CurrentTime
+
+  cubeMesh.rotation.y += THREE.MathUtils.degToRad(1)*delta*20
+  cubeMesh.scale.x = (Math.sin(CurrentTime))*2+2
+  cubeMesh.position.x = (Math.sin(CurrentTime))+2
   controls.update()
-  renderer.render(scene,camera)
   window.requestAnimationFrame(renderloop)
+  renderer.render(scene,camera)
   
 }
 
